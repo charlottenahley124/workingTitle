81d621696bb8ae7d730a9547215ccb34f5f04d40
Julian KG <theaceshinigami@Julians-MacBook-Air.local>
2017-05-19 19:24:25 -0400

    msg

--- ui.lua
+++ ui.lua
@@ -3,7 +3,7 @@ local game = require "game"
 local drawGrid = function ()
   for i,v in ipairs(game.map) do
     for j,w in ipairs(v) do
-      if w then
+      if w ~= 0 then
         love.graphics.rectangle("line", j*32, i*32, 32, 32)
       end
     end
